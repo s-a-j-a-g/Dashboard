6cd6f044d8dc2a6a7b9640bc16e0f0b0d971a733
s_a_j_a_g <spd.sajag@gmail.com>
2023-12-11 13:15:31 +0545

    initialize UI

--- dashboard.py
+++ dashboard.py
@@ -12,3 +12,50 @@ st.title(" :bar_chart: Sample Superstore EDA")
 st.markdown(
     "<style>div.block-container{padding-top:1rem;}</style>", unsafe_allow_html=True
 )
+
+fl = st.file_uploader(
+    ":file_folder: Uplaod a File", type=(["csv", "txt", "xlsx", "xls"])
+)
+
+if fl is not None:
+    fileName = fl.name
+    st.write(fileName)
+    df = pd.read_csv(fileName, encoding="ISO-8859-1")
+else:
+    os.chdir(r"C:\Users\DELL\Documents\DevelopmentFiles\Data-Visualization")
+    df = pd.read_csv("superstore.csv", encoding="ISO-8859-1")
+
+col1, col2 = st.columns((2))
+df["Order Date"] = pd.to_datetime(df["Order Date"])
+
+# Get the min and max date
+startDate = pd.to_datetime(df["Order Date"]).min()
+endDate = pd.to_datetime(df["Order Date"]).max()
+
+with col1:
+    date1 = pd.to_datetime(st.date_input("Start Date", startDate))
+
+with col2:
+    date2 = pd.to_datetime(st.date_input("End Date", endDate))
+
+
+df = df[(df["Order Date"] >= date1) & (df["Order Date"] <= date2)].copy()
+
+st.sidebar.header("Choose your filter: ")
+
+# Region
+region = st.sidebar.multiselect("Pick your region", df["Region"].unique())
+if not region:
+    df2 = df.copy()
+else:
+    df2 = df[df["Region"].isin(region)]
+
+# State
+state = st.sidebar.multiselect("Pick your state", df2["State"].unique())
+if not state:
+    df3 = df2.copy()
+else:
+    df3 = df2[df2["State"].isin(state)]
+
+# City
+city = st.sidebar.multiselect("Pick your city", df3["City"].unique())
